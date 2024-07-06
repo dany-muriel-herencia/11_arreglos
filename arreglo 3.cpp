4893aaf0ac754eca411deea578999e745aace89e
dany-muriel-herencia <drmurielh@unjbg.edu.pe>
2024-07-05 22:39:26 -0500

    alfin acabe

--- arreglo 3.cpp
+++ arreglo 3.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main(){
     int b, g = 0, e = 0; 
-
+    int menor[1];
     int temperaturaminima[7];
     int temperaturamaxima[7];
     int temperaturapromedio[7];
@@ -27,5 +27,30 @@ int main(){
     for (int i = 0; i < 7; ++i) {
         cout << temperaturapromedio[i] << " ";
     }
+    menor[1]=23;
+    for (int i = 0; i < 7; ++i) {
+           
+        if(temperaturaminima[i] < menor[1]){
+            menor[1]=temperaturaminima[i];
+        }
+    }
+    cout<<"la menor temperatura es "<<menor[1]<<endl;
+    //la ultima funcion comparacion
+
+    cout << "Ingrese una temperatura para comparar: ";
+    cin >> e;
+    bool encontrado = false;
+    for (int i = 0; i < 7; ++i) {
+        if (temperaturamaxima[i] == e) {
+            cout << "El día " << i + 1 << " tiene una temperatura máxima de " << e << "°C" << endl;
+            encontrado = true;
+        }
+    }
+    if (!encontrado) {
+        cout << "No hay ningún día con una temperatura máxima de " << e << "°C" << endl;
+    }
+
+
+
     return 0;
     }
