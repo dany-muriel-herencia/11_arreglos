bc4d833e4d21e6018e4033f3e648397c062f1f44
dany-muriel-herencia <drmurielh@unjbg.edu.pe>
2024-07-06 15:20:58 -0500

    acobe el primer menu

---
+++ arreglo 4.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+void creciente(int a,int b);
+
+int main(){
+
+    int a,b;
+    cout<<"indique el tamaño del arreglo : " ;cin>>a;
+    cout<<"ingrese un digito : ";cin>>b;
+    creciente(a,b);
+    return 0;
+
+}
+void creciente(int a,int b){
+    int c,d,g,j=0,arreglo[a];
+    for(int i=0;i<a;i++){
+        j++;
+        c=j*b;
+        cout<<"primer elemento  "<<i+1<<" : "<<arreglo[i]<<endl;
+        
+    }
+
+}
+
